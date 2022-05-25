1e35c6b4392047123e6338e4b23f888021290728
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-05-25 14:44:34 -0700

    add a few event listeners for buttons

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+/*-----------------------------------
+--------Add and Remove Books---------
+-----------------------------------*/
+const addBookBtn = document.getElementById('add-book-btn');
+
+function addBook() {
+    addBookBtn.addEventListener('click', () => {
+        alert();
+    })
+}
+
+const removeBookBtn = document.getElementById('remove-book-btn');
+function deleteBook() {
+    removeBookBtn.addEventListener('click', () => {
+        console.log("Delete Book")
+    })
+}
+
+
+
+
+
+
+let myLibrary = [];
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -8,6 +32,13 @@ function Book(title, author, pages, read) {
     }
 }
 
+function addBookToLibrary() {
+
+}
+
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'read');
 
 console.log(theHobbit.info());
+
+deleteBook();
+addBook();
