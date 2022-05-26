13c52581e7a79338dda92ff2c15abc968fd3d1bd
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-05-26 09:31:45 -0700

    add new book to array whne you click submit

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
+let myLibrary = [];
+
 /*-----------------------------------
 --------    Add New Books   ---------
 -----------------------------------*/
 const addBookBtn = document.getElementById('add-book-btn');
 const addBookPopup = document.getElementById('add-book-popup');
-const closeAddBook = document.getElementById('close-add-book-popup-btn');
-
 function addBook() {
     addBookBtn.addEventListener('click', () => {
         addBookPopup.showModal();
     })
 }
 
+const submitBookBtn = document.getElementById('submit-book-btn');
+function checkForSubmitBtn() {
+    submitBookBtn.addEventListener('click', () => {
+        addBookToLibrary();
+    })
+}
+function addBookToLibrary() {
+    if (document.getElementById('book-read').checked == true) {
+        let book = new ReadBook(
+            document.getElementById("book-title").value, 
+            document.getElementById("book-author").value, 
+            document.getElementById("book-pages").value,
+            document.getElementById('book-rating').value
+        );
+        myLibrary.push(book);
+        console.log(myLibrary);
+    }
+    if (document.getElementById('book-unread').checked == true){
+        let book = new UnreadBook(
+            document.getElementById("book-title").value, 
+            document.getElementById("book-author").value, 
+            document.getElementById("book-pages").value,
+        );
+        myLibrary.push(book);
+        console.log(myLibrary);
+    }
+}
+
+const closeAddBook = document.getElementById('close-add-book-popup-btn');
 function closeAddBookPopup() {
     closeAddBook.addEventListener('click', () => {
         addBookPopup.close();
@@ -50,26 +79,28 @@ function deleteBookBtnEvent() {
 }
 
 
-
-
-
-
-let myLibrary = [];
-function Book(title, author, pages, read) {
+function UnreadBook(title, author, pages) {
     this.title = title
     this.author = author
     this.pages = pages
-    this.read = read
+    this.read = "Unread"
     this.info = function() {
         return `"${title} by ${author}, ${pages} pages, ${read}"`
     }
 }
 
-function addBookToLibrary() {
-
+function ReadBook(title, author, pages, rating) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = "Read"
+    this.rating = rating
+    this.info = function() {
+        return `"${title} by ${author}, ${pages} pages, ${rating}"`
+    }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'read');
+const theHobbit = new ReadBook('The Hobbit', 'J.R.R. Tolkien', '295', '9');
 
 console.log(theHobbit.info());
 
@@ -77,3 +108,4 @@ console.log(theHobbit.info());
 closeAddBookPopup();
 deleteBookBtnEvent();
 addBook();
+checkForSubmitBtn();
